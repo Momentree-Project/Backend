242216f2e7de1fc82eb541819477e7cbbe8dd324
ksuju <139477852+ksuju@users.noreply.github.com>
2025-04-16 19:15:43 +0900

    init

---
+++ src/test/java/com/momentree/MomentreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momentree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
