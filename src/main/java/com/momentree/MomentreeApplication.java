fa8482d0526a362d1e35eda38292f73965990b00
eunoia73 <soo97703@naver.com>
2025-04-18 22:39:15 +0900

    feat: BaseEntity 설정

--- src/main/java/com/momentree/MomentreeApplication.java
+++ src/main/java/com/momentree/MomentreeApplication.java
@@ -2,8 +2,10 @@ package com.momentree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MomentreeApplication {
 
 	public static void main(String[] args) {
