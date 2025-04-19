7a2a038f73941516759c558376d106d3184c26c3
eunoia73 <soo97703@naver.com>
2025-04-19 11:44:00 +0900

    feat: Category 엔티티 추가

---
+++ src/main/java/com/momentree/domain/category/entity/Category.java
@@ -0,0 +1,42 @@
+package com.momentree.domain.category.entity;
+
+import com.momentree.domain.category.constant.CategoryColor;
+import com.momentree.domain.category.constant.CategoryType;
+import com.momentree.domain.datemarker.entity.DateMarkerCategory;
+import com.momentree.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.envers.AuditOverride;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "CATEGORY")
+@AuditOverride(forClass = BaseEntity.class)
+@AttributeOverride(name = "id", column = @Column(name = "category_id"))
+public class Category extends BaseEntity {
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "memo")
+    private String memo;
+
+    @Column(name = "color", nullable = false)
+    private CategoryColor categoryColor;
+
+    @Column(name = "type", nullable = false)
+    private CategoryType categoryType;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<DateMarkerCategory> dateMarkers = new ArrayList<>();
+
+}
