c2873fe1ea6f173132417a96990d4340ea289c71
ksuju <139477852+ksuju@users.noreply.github.com>
2025-04-19 12:09:01 +0900

    feat: Like 엔티티 생성

---
+++ src/main/java/com/momentree/domain/like/entity/Like.java
@@ -0,0 +1,31 @@
+package com.momentree.domain.like.entity;
+
+import com.momentree.domain.post.post.entity.Post;
+import com.momentree.domain.user.entity.User;
+import com.momentree.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Table(name = "like")
+@AttributeOverride(name = "id", column = @Column(name = "like_id"))
+public class Like extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+}
