c5a2c857d579e6cb95636cfea8431912560a0f35
ksuju <139477852+ksuju@users.noreply.github.com>
2025-04-19 11:03:26 +0900

    feat: Comment 엔티티 추가

---
+++ src/main/java/com/momentree/domain/post/comment/entity/Comment.java
@@ -0,0 +1,45 @@
+package com.momentree.domain.post.comment.entity;
+
+import com.momentree.domain.post.post.entity.Post;
+import com.momentree.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * packageName    : com.momentree.domain.post.comment.entity
+ * fileName       : comment
+ * author         : sungjun
+ * date           : 2025-04-19
+ * description    : 자동 주석 생성
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-04-19        kyd54       최초 생성
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Table(name = "comment")
+@AttributeOverride(name = "id", column = @Column(name = "comment_id"))
+public class Comment extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private String content;
+
+    private int level;
+
+}
