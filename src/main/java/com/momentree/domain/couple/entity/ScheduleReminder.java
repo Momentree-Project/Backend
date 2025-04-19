456108271bfec245b4fbe1781e93bc0b9fb3e0a6
ksuju <139477852+ksuju@users.noreply.github.com>
2025-04-19 14:23:05 +0900

    feat: ScheduleReminder 엔티티 생성

---
+++ src/main/java/com/momentree/domain/couple/entity/ScheduleReminder.java
@@ -0,0 +1,31 @@
+package com.momentree.domain.couple.entity;
+
+import com.momentree.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Table(name = "schedule_reminder")
+@AttributeOverride(name = "id", column = @Column(name = "schedule_reminder_id"))
+
+public class ScheduleReminder extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+
+    @Column(name = "notify_time")
+    private LocalDateTime notifyTime;
+
+}
