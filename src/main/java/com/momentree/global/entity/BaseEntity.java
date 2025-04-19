f9606c0374d93b59a0cf79e2c861a20e74afe0ea
eunoia73 <soo97703@naver.com>
2025-04-19 14:13:29 +0900

    refactor: AuditingEntityListener 사용 방식 제거

--- src/main/java/com/momentree/global/entity/BaseEntity.java
+++ src/main/java/com/momentree/global/entity/BaseEntity.java
@@ -1,6 +1,5 @@
 package com.momentree.global.entity;
 
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
@@ -11,7 +10,6 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -23,7 +21,6 @@ import static lombok.AccessLevel.PROTECTED;
 @AllArgsConstructor(access = PROTECTED)
 @SuperBuilder
 @MappedSuperclass
-@EntityListeners(value = {AuditingEntityListener.class})
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public abstract class BaseEntity {
     @Id
