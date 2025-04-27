8277488f675aaeb87fb06e97ab4abd4e7d389c22
ksuju <139477852+ksuju@users.noreply.github.com>
2025-04-27 20:56:45 +0900

    feat: AppConfig 설명 주석 추가

--- src/main/java/com/momentree/global/app/AppConfig.java
+++ src/main/java/com/momentree/global/app/AppConfig.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+// 사이트 URL, 쿠키 도메인 등 환경설정값을 관리하는 AppConfig 클래스
 @Configuration
 public class AppConfig {
     @Getter
