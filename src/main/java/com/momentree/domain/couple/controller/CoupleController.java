53c0fe2fee6cce75915795eea9d50ff95be5367a
eunoia73 <soo97703@naver.com>
2025-05-03 22:51:47 +0900

    feat: #24 커플 연결 controller 추가

---
+++ src/main/java/com/momentree/domain/couple/controller/CoupleController.java
@@ -0,0 +1,28 @@
+package com.momentree.domain.couple.controller;
+
+import com.momentree.domain.auth.oauth2.CustomOAuth2User;
+import com.momentree.domain.couple.dto.request.CoupleConnectRequestDto;
+import com.momentree.domain.couple.dto.response.CoupleConnectResponseDto;
+import com.momentree.domain.couple.service.CoupleService;
+import com.momentree.global.exception.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/couple")
+public class CoupleController {
+
+    private final CoupleService coupleService;
+
+    @PostMapping("/connect")
+    public BaseResponse<CoupleConnectResponseDto> connectCouple(
+            @AuthenticationPrincipal CustomOAuth2User userDetails,
+            @RequestBody CoupleConnectRequestDto requestDto){
+        Long userId = userDetails.getUserId();
+        CoupleConnectResponseDto connectResponse = coupleService.connectCouple(userId, requestDto);
+        return new BaseResponse<>(connectResponse);
+    }
+
+}
