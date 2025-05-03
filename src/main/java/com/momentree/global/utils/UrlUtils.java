47a6d3b43542d4f4259d6ec40af350ff55bd44f2
ksuju <139477852+ksuju@users.noreply.github.com>
2025-05-03 20:59:41 +0900

    feat: 테스트를 위해 임시로 모든 API URL 허용 (나중에 삭제)

--- src/main/java/com/momentree/global/utils/UrlUtils.java
+++ src/main/java/com/momentree/global/utils/UrlUtils.java
@@ -10,6 +10,9 @@ public class UrlUtils {
             "/images/**",
             "/auth/**",  // 로그인/회원가입 관련 URL
             "/oauth2/**", // OAuth2 관련 URL
-            "/api/v1/auth/refresh-token"
+            "/api/v1/auth/refresh-token",
+
+            // 테스트를 위해 임시로 모든 URL 허용 (나중에 삭제)
+            "/api/v1/**", // 모든 API URL
     };
 }
