31d205ee4ccd0b4b7a5e6da55d839b9306173f4c
ksuju <139477852+ksuju@users.noreply.github.com>
2025-05-03 20:57:42 +0900

    feat: schedule 관련 에러코드 추가

--- src/main/java/com/momentree/global/exception/ErrorCode.java
+++ src/main/java/com/momentree/global/exception/ErrorCode.java
@@ -19,6 +19,8 @@ public enum ErrorCode {
      * 400 : Request, Response 오류
      */
     NOT_FOUND_USER(HttpStatus.NOT_FOUND.value(), "일치하는 유저가 없습니다."),
+    NOT_FOUND_COUPLE(HttpStatus.NOT_FOUND.value(), "일치하는 커플이 없습니다."),
+    NOT_FOUND_CATEGORY(HttpStatus.NOT_FOUND.value(), "일치하는 카테고리가 없습니다."),
 
     /**
      * 400 : Validation Error
@@ -27,6 +29,8 @@ public enum ErrorCode {
 
     INVALID_JWT(HttpStatus.UNAUTHORIZED.value(), "유효하지 않은 JWT입니다."),
     INVALID_AUTHORIZATION(HttpStatus.CONFLICT.value(), "잘못된 권한입니다."),
+    INVALID_SCHEDULE_DATA(HttpStatus.BAD_REQUEST.value(), "잘못된 일정 데이터가 제공되었습니다."),
+
 
 
     /**
@@ -34,7 +38,9 @@ public enum ErrorCode {
      */
     FAILED_TO_CONNECT_DATABASE(HttpStatus.INTERNAL_SERVER_ERROR.value(), "데이터베이스 연결에 실패하였습니다."),
     FAILED_TO_CONNECT_SERVER(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버와의 연결에 실패하였습니다."),
-    UNEXPECTED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "예상치 못한 에러가 발생했습니다.");
+    UNEXPECTED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "예상치 못한 에러가 발생했습니다."),
+    FAILED_CREATE_SCHEDULE(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버 내부 오류로 인해 스케줄 생성에 실패했습니다."),
+    ;
 
     private final int code;
     private final String message;
