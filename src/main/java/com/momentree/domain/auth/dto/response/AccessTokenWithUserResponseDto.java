4e46f1c6f6334ddb6d36466bbac7b575faeb96dc
eunoia73 <soo97703@naver.com>
2025-05-06 10:02:44 +0900

    fix: #28 반환하는 User 정보에 birth 추가

--- src/main/java/com/momentree/domain/auth/dto/response/AccessTokenWithUserResponseDto.java
+++ src/main/java/com/momentree/domain/auth/dto/response/AccessTokenWithUserResponseDto.java
@@ -1,20 +1,25 @@
 package com.momentree.domain.auth.dto.response;
 
+import java.time.LocalDate;
+
 public record AccessTokenWithUserResponseDto(String accessToken,
                                              String username,
                                              String email,
                                              String userCode,
+                                             LocalDate birth,
                                              Long coupleId
 ) {
     public AccessTokenWithUserResponseDto(String accessToken,
                                           String username,
                                           String email,
                                           String userCode,
+                                          LocalDate birth,
                                           Long coupleId) {
         this.accessToken = accessToken;
         this.username = username;
         this.email = email;
         this.userCode = userCode;
+        this.birth = birth;
         this.coupleId = coupleId;
     }
 }
