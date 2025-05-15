84872ff9aafe76768ef5547e3c9da31ff0ce9882
ksuju <139477852+ksuju@users.noreply.github.com>
2025-05-15 20:28:34 +0900

    feat: #41 Post 엔티티에 Couple 연관관계 추가

--- src/main/java/com/momentree/domain/post/post/entity/Post.java
+++ src/main/java/com/momentree/domain/post/post/entity/Post.java
@@ -1,5 +1,6 @@
 package com.momentree.domain.post.post.entity;
 
+import com.momentree.domain.couple.entity.Couple;
 import com.momentree.domain.post.post.constant.PostStatus;
 import com.momentree.domain.user.entity.User;
 import com.momentree.global.entity.BaseEntity;
@@ -17,10 +18,13 @@ import lombok.experimental.SuperBuilder;
 @Table(name = "post")
 @AttributeOverride(name = "id", column = @Column(name = "post_id"))
 public class Post extends BaseEntity {
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "couple_id")
+    private Couple couple;
 
     @Column(name = "content", nullable = false)
     private String content;
