ab34c0aa5f132aec5c5401f4cfd142c41a877376
ksuju <139477852+ksuju@users.noreply.github.com>
2025-05-22 17:24:08 +0900

    feat: #41 이미지 업로드 전략 패턴 적용

--- src/main/java/com/momentree/global/service/S3Service.java
+++ src/main/java/com/momentree/global/service/S3Service.java
@@ -1,11 +1,16 @@
 package com.momentree.global.service;
 
+import com.momentree.domain.post.post.entity.Post;
+import com.momentree.domain.user.entity.User;
+import com.momentree.global.constant.FileType;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 public interface S3Service {
-    String uploadProfileImage(MultipartFile file, Long userId) throws IOException;
-
+    // 프로필 이미지용
+    String uploadImage(MultipartFile file, FileType strategyType, Long userId) throws IOException;
+    // 게시글 이미지용
+    String uploadImage(MultipartFile file, FileType strategyType, User user, Post post) throws IOException;
     void deleteProfileImage(Long userId);
 }
