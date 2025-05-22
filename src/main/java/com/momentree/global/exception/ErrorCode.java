1021db5b0efa7ea177165925faa0943726e6aa93
ksuju <139477852+ksuju@users.noreply.github.com>
2025-05-22 16:23:00 +0900

    feat: #41 이미지 관련 에러코드 추가

--- src/main/java/com/momentree/global/exception/ErrorCode.java
+++ src/main/java/com/momentree/global/exception/ErrorCode.java
@@ -24,6 +24,7 @@ public enum ErrorCode {
     NOT_FOUND_SCHEDULE(HttpStatus.NOT_FOUND.value(), "일치하는 스케줄이 없습니다."),
     NOT_FOUND_PARTNER(HttpStatus.NOT_FOUND.value(), "연결된 파트너를 찾을 수 없습니다."),
     NOT_FOUND_POST(HttpStatus.NOT_FOUND.value(), "일치하는 게시글이 없습니다."),
+    NOT_FOUND_STRATEGY_NAME(HttpStatus.NOT_FOUND.value(), "일치하는 전략패턴이 없습니다."),
     ALREADY_CONNECTED_COUPLE(HttpStatus.BAD_REQUEST.value(), "이미 커플이 연결된 상태입니다."),
     CANNOT_CONNECT_SELF(HttpStatus.BAD_REQUEST.value(), "자기 자신과는 커플 연결을 할 수 없습니다."),
     NOT_CONNECTED_COUPLE(HttpStatus.BAD_REQUEST.value(), "커플이 연결되지 않은 상태입니다."),
@@ -38,6 +39,7 @@ public enum ErrorCode {
     INVALID_AUTHORIZATION(HttpStatus.CONFLICT.value(), "잘못된 권한입니다."),
     INVALID_SCHEDULE_DATA(HttpStatus.BAD_REQUEST.value(), "잘못된 일정 데이터가 제공되었습니다."),
     INVALID_FILE_EXTENSION(HttpStatus.BAD_REQUEST.value(), "지원하지 않는 파일 형식입니다."),
+    INVALID_FILE_TYPE(HttpStatus.BAD_REQUEST.value(), "용도에 맞지 않는 FileType 입니다."),
 
 
     /**
