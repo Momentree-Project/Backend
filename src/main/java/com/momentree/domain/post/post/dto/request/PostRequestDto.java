626127adc7e47e1d458e52a96bde8a7007c15469
ksuju <139477852+ksuju@users.noreply.github.com>
2025-05-22 17:22:03 +0900

    feat: #41 게시글 작성 시 이미지 업로드 기능 추가를 위해 PostRequestDto 수정

--- src/main/java/com/momentree/domain/post/post/dto/request/PostRequestDto.java
+++ src/main/java/com/momentree/domain/post/post/dto/request/PostRequestDto.java
@@ -1,6 +1,16 @@
 package com.momentree.domain.post.post.dto.request;
 
+import com.momentree.global.constant.FileType;
+import org.springframework.web.multipart.MultipartFile;
+
 public record PostRequestDto(
-        String content
+        String content,
+        MultipartFile image,
+        FileType fileType
 ) {
+    public PostRequestDto {
+        if (fileType == null) {
+            fileType = FileType.POST;
+        }
+    }
 }
