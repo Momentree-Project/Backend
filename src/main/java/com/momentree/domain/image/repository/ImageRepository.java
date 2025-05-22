3ff4e3914f60fb2152b3112ee611a82bffc7be88
ksuju <139477852+ksuju@users.noreply.github.com>
2025-05-22 17:21:30 +0900

    feat: #41 게시글을 불러올 때 이미지 URL을 같이 가져오기 위해 ImageRepository에 findByPostIdIn 메서드 추가

--- src/main/java/com/momentree/domain/image/repository/ImageRepository.java
+++ src/main/java/com/momentree/domain/image/repository/ImageRepository.java
@@ -4,8 +4,11 @@ import com.momentree.domain.image.entity.Image;
 import com.momentree.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
     Optional<Image> findByUserAndPostIsNull(User user);
+    List<Image> findByPostIdIn(Collection<Long> postIds);
 }
