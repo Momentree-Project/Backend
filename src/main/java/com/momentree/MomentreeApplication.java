86acce8685c03def071fc586644454f18d059042
ksuju <139477852+ksuju@users.noreply.github.com>
2025-05-25 22:56:53 +0900

    fix: 시큐리티 주석 해제 및 재활성화

--- src/main/java/com/momentree/MomentreeApplication.java
+++ src/main/java/com/momentree/MomentreeApplication.java
@@ -2,13 +2,9 @@ package com.momentree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-		org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration.class,
-})
+@SpringBootApplication
 @EnableJpaAuditing
 public class MomentreeApplication {
 
