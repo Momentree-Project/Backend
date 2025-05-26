f5eb6beec10235c59bbe06c23047fe0a36bc9f5a
ksuju <139477852+ksuju@users.noreply.github.com>
2025-05-26 23:13:07 +0900

    feat: #41 클라이언트에서 게시글 작성자의 프로필이 표시되도록 responseDto 수정

--- src/main/java/com/momentree/domain/image/repository/ImageRepository.java
+++ src/main/java/com/momentree/domain/image/repository/ImageRepository.java
@@ -4,6 +4,8 @@ import com.momentree.domain.image.entity.Image;
 import com.momentree.domain.post.post.entity.Post;
 import com.momentree.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
@@ -14,4 +16,8 @@ public interface ImageRepository extends JpaRepository<Image, Long> {
     List<Image> findByPostIdIn(Collection<Long> postIds);
     List<Image> findByPost(Post post);
     Optional<Image> findByImageUrlAndPost(String imageUrl, Post post);
+
+    // 여러 사용자의 프로필 이미지를 한 번에 조회
+    @Query("SELECT i FROM Image i WHERE i.user.id IN :userIds AND i.post IS NULL")
+    List<Image> findProfileImagesByUserIds(@Param("userIds") List<Long> userIds);
 }
