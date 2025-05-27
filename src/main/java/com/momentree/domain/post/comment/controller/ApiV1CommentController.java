67eedc3b7f96b02805067816fdee39b8efd13116
ksuju <139477852+ksuju@users.noreply.github.com>
2025-05-27 20:19:03 +0900

    feat: #41 댓글/대댓글 삭제 기능 추가

--- src/main/java/com/momentree/domain/post/comment/controller/ApiV1CommentController.java
+++ src/main/java/com/momentree/domain/post/comment/controller/ApiV1CommentController.java
@@ -5,6 +5,7 @@ import com.momentree.domain.post.comment.dto.request.PostCommentRequest;
 import com.momentree.domain.post.comment.dto.response.PostCommentResponse;
 import com.momentree.domain.post.comment.service.CommentService;
 import com.momentree.global.exception.BaseResponse;
+import com.momentree.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -31,5 +32,14 @@ public class ApiV1CommentController {
             @PathVariable("post-id") Long postId
     ) {
         return new BaseResponse<>(commentService.getAllComments(loginUser, postId));
+    }
+
+    @DeleteMapping("{comment-id}")
+    public BaseResponse<Void> deleteComment(
+            @AuthenticationPrincipal CustomOAuth2User loginUser,
+            @PathVariable("comment-id") Long commentId
+    ) {
+        commentService.deleteComment(loginUser, commentId);
+        return new BaseResponse<>(ErrorCode.SUCCESS);
     }
 }
