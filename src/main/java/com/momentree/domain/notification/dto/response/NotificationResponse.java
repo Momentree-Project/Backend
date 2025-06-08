98880699fc8009682c7cc611e3aa27956bb4f2bc
ksuju <139477852+ksuju@users.noreply.github.com>
2025-06-08 19:07:00 +0900

    feat: #82 알림 Response 완성

--- src/main/java/com/momentree/domain/notification/dto/response/NotificationResponse.java
+++ src/main/java/com/momentree/domain/notification/dto/response/NotificationResponse.java
@@ -1,6 +1,26 @@
 package com.momentree.domain.notification.dto.response;
 
-public record NotificationResponse(
+import com.momentree.domain.notification.constant.NotificationType;
+import com.momentree.domain.notification.entity.Notification;
 
+import java.time.LocalDateTime;
+
+public record NotificationResponse (
+    Long id,
+    String content,
+    String redirectUrl,
+    NotificationType type,
+    LocalDateTime createdAt
 ) {
+    public static NotificationResponse from(
+            Notification notification
+    ) {
+        return new NotificationResponse(
+                notification.getId(),
+                notification.getContent(),
+                notification.getRedirectUrl(),
+                notification.getType(),
+                notification.getCreatedAt()
+        );
+    }
 }
